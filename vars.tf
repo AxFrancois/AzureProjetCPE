a8940f626298bd3d0789598ea282c152d9eebfaf
Axel <axel.francois-charlot@cpe.fr>
2023-10-13 13:45:34 +0200

    another test

--- vars.tf
+++ vars.tf
@@ -1,3 +1,19 @@
 variable "region" {
-  default = "us-east-1"
+  default = "France central"
+}
+
+variable "tenant_id" {
+  default = "ce4775d3-1c2e-4644-b2bc-631c157d2be7"
+}
+
+variable "subscription_id" {
+  default = "77bb0b07-57e5-44b5-a350-310a93917298"
+}
+
+variable "client_id" {
+  default = "88b89f8b-ffc6-4d7a-8d6e-1e59338c6844"
+}
+
+variable "client_secret" {
+  sensitive = true
 }
