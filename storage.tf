4a75006657c8a600f0cd2def8c432942a0a19fe6
Axel <axel.francois-charlot@cpe.fr>
2023-10-16 14:01:59 +0200

    correction names

--- storage.tf
+++ storage.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "ConfidentielStorageAccount" {
-  name                     = "ConfidentielStorageAccount"
+  name                     = "confidentiel"
   resource_group_name      = azurerm_resource_group.RG_CPE.name
   location                 = azurerm_resource_group.RG_CPE.location
   account_tier             = "Standard"
@@ -7,7 +7,7 @@ resource "azurerm_storage_account" "ConfidentielStorageAccount" {
 }
 
 resource "azurerm_storage_share" "CondidentialServices-share-1" {
-  name                 = "Confidentiel"
+  name                 = "confidentiel"
   storage_account_name = azurerm_storage_account.ConfidentielStorageAccount.name
   quota                = 250
 }
