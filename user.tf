4f4d79086dd7878d30a412247af110b4e3e8c869
Alex <alexandre.drevet1@gmail.com>
2023-10-16 14:17:01 +0200

    End of the creation of users

--- user.tf
+++ user.tf
@@ -42,3 +42,37 @@ resource "azuread_user" "AF-5" {
   company_name        = "CPE"
   department          = "Administratif"
 }
+
+resource "azuread_group" "IT" {
+  display_name = "IT"
+  security_enabled = true
+}
+
+resource "azuread_group" "AF" {
+  display_name = "Administratif"
+  security_enabled = true
+}
+
+resource "azuread_group_member" "IT" {
+  group_object_id = azuread_group.IT.id
+  member_object_id = [azuread_user.IT-1.id, azuread_user.IT-2.id, azuread_user.IT-3.id]
+}
+
+resource "azuread_group_member" "AF" {
+  group_object_id = azuread_group.AF.id
+  member_object_id = [azuread_user.AF-4.id, azuread_user.AF-5.id]
+}
+
+resource "azuread_administrative_unit" "Admin_unit" {
+  display_name = "Admin_unit"
+}
+
+resource "azuread_directory_role" "Admin" {
+  display_name = "Administrator"
+}
+
+resource "azuread_administrative_unit_role_member" "Administrator" {
+  role_object_id = azuread_directory_role.Admin.object_id
+  administrative_unit_object_id = azuread_administrative_unit.Admin_unit.id
+  member_object_id = [azuread_user.IT-1.id, azuread_user.IT-2.id, azuread_user.IT-3.id]
+}
